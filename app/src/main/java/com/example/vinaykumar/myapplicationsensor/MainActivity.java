eceae9209a0a7bb099ee8ac375bd34f95cbb84ba
Vinay206 <k.vinaykumar51@gmail.com>
2016-06-19 06:34:42 +0530

    Initial commit

---
+++ app/src/main/java/com/example/vinaykumar/myapplicationsensor/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.vinaykumar.myapplicationsensor;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.*;
+
+
+public class MainActivity extends Activity implements SensorEventListener {
+
+    private SensorManager senSensorManager;
+    private Sensor senAccelerometer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        senSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        senAccelerometer = senSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        senSensorManager.registerListener(this, senAccelerometer , SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        TextView textViewX = (TextView)findViewById(R.id.textViewX);
+        TextView textViewY = (TextView)findViewById(R.id.textViewY);
+        TextView textViewZ = (TextView)findViewById(R.id.textViewZ);
+
+        Sensor mySensor = sensorEvent.sensor;
+
+        if (mySensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            float x = sensorEvent.values[0];
+            float y = sensorEvent.values[1];
+            float z = sensorEvent.values[2];
+
+            textViewX.setText("X: " + String.valueOf(x));
+            textViewY.setText("Y: " + String.valueOf(y));
+            textViewZ.setText("Z: " + String.valueOf(z));
+
+
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+    protected void onPause() {
+        super.onPause();
+        senSensorManager.unregisterListener(this);
+    }
+    protected void onResume() {
+        super.onResume();
+        senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+}
